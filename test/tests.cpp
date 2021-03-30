94a6d0c03fe508d6004f25f7c1eb34a710c7d981
Wortellen <72662214+wortellen@users.noreply.github.com>
2021-03-31 00:35:50 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -37,31 +37,4 @@ TEST(task2, test4) {
     EXPECT_EQ(4, result);
 }
 
-TEST(task3, test5) {
-    a.on();
-    a.coin(100);
-    a.choice(2);
-    a.coin(20);
-    a.choice(2);
-    int result = a.getState();
-    a.finish();
-    EXPECT_EQ(1, result);
-}
 
-TEST(task3, test6) {
-    Automata a = Automata();
-    a.on();
-    a.coin(150);
-    a.choice(0);
-    a.finish();
-    int result=a.cancel();
-    EXPECT_EQ(70, result);
-}
-TEST(task3, test7) {
-    Automata a = Automata();
-    a.choice(5);
-    a.choice(0);
-    a.coin(150);
-    int result = a.choice(1);
-    EXPECT_EQ(0, result);
-}

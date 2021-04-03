0f08dd49768b65e298ef8182f52d1e50f007759d
FoxedFox <71232184+FoxedFox@users.noreply.github.com>
2021-04-03 15:49:49 +0300

    Add files via upload

---
+++ src/Automata.cpp
@@ -0,0 +1,58 @@
+﻿#include "Automata.h"
+#include <string>
+void Automata::check(unsigned int ch) {
+	if (cash >= prices[ch]) {
+		cash -= prices[ch];
+		cook();
+	}
+	else {
+		cancel();
+	}
+}
+
+void Automata::on() {
+	cash = 0;
+	state = WAIT;
+}
+
+void Automata::off() {
+	state = OFF;
+}
+
+void Automata::coin(unsigned int money) {
+	cash += money;
+	state = ACCEPT;
+}
+
+const string* Automata::etMenu()
+{
+	return menu;
+}
+
+const STATE Automata::getState()
+{
+	return state;
+}
+
+void Automata::choice(unsigned int ch) {
+	if (ch - 1 < 3) {
+		state = CHECK;
+		check(ch);
+	}
+}
+
+void Automata::cancel() {
+	cash = 0;
+	state = WAIT;
+}
+
+void Automata::cook() {
+	state = COOK;
+	is_coocking = true;
+	finish();
+}
+
+void Automata::finish() {
+	is_coocking = false;
+	state = WAIT;
+}

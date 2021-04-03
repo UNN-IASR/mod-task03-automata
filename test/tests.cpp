f05f99d09a26f41052f5d143a4e0be46d6980d65
FoxedFox <71232184+FoxedFox@users.noreply.github.com>
2021-04-03 15:27:50 +0300

    Add files via upload

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,30 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
+TEST(task, test1)
+{
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.coin(20);
+    coffee.choise(1);
+    States result = coffee.getState();
+    EXPECT_EQ(2, result);
+}
+
+TEST(task, test2)
+{
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.choise(1);
+    States result = coffee.getState();
+    EXPECT_EQ(3, result);
+}
+
+TEST(task, test3)
+{
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.coin(30);
+    coffee.choise(2);
+    int result = coffee.finish();
+    EXPECT_EQ(1, result);
+}

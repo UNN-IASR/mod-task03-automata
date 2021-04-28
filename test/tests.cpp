6f5c2471801a817043263271a77f808eb82f180a
Aniloren <80814419+Aniloren@users.noreply.github.com>
2021-04-28 12:01:02 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,64 +1,70 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
 
-TEST(task1, test1) 
-{
-  Automata A;
-  A.cancel();
-  int result = A.getState();
-  EXPECT_EQ(0, result);
+include <gtest/gtest.h>
+#include "Automata.h"
+TEST(task1, test1) {
+    Automata a = Automata();
+    a.on();
+    a.coin(100);
+    int result = a.getState();
+    EXPECT_EQ(2, result);
 }
 
-TEST(task1, test2) 
-{
-  Automata A;
-  A.on();
-  A.coin(0);
-  int result = A.getState();
-  EXPECT_EQ(2, result);
+TEST(task1, test2) {
+    Automata a = Automata();
+    a.on();
+    a.coin(100);
+    a.choice(2);
+    int result = a.getState();
+    EXPECT_EQ(2, result);
 }
 
-TEST(task2, test3) 
-{
-  Automata A;
-  A.on();
-  A.cancel();
-  int result = A.getState();
-  EXPECT_EQ(1, result);
+TEST(task2, test3) {
+    Automata a = Automata();
+    a.on();
+    a.coin(100);
+    a.choice(2);
+    int result = a.getState();
+    EXPECT_EQ(2, result);
 }
 
-TEST(task2, test4) 
-{
-  Automata A;
-  A.on();
-  A.coin(100);
-  int result = A.choice(-1);
-  EXPECT_EQ(0, result);
+TEST(task2, test4) {
+    Automata a = Automata();
+    a.on();
+    a.coin(100);
+    a.choice(2);
+    a.coin(20);
+    a.choice(2);
+    int result = a.getState();
+    EXPECT_EQ(4, result);
 }
 
-TEST(task3, test5) 
-{
-  Automata A;
-  A.on();
-  A.coin(100);
-  int result = A.choice(2);
-  EXPECT_EQ(0, result);
+TEST(task3, test5) {
+    a.on();
+    a.coin(100);
+    a.choice(2);
+    a.coin(20);
+    a.choice(2);
+    int result = a.getState();
+    a.finish();
+    EXPECT_EQ(1, result);
 }
 
-TEST(task3, test6) 
-{
-  Automata A;
-	A.on();
-	A.coin(100);
-	int result = A.cancel();
-  EXPECT_EQ(100, result);
+TEST(task3, test6) {
+    Automata a = Automata();
+    a.on();
+    a.coin(150);
+    a.choice(0);
+    a.finish();
+    int result=a.cancel();
+    EXPECT_EQ(70, result);
 }
-
-TEST(task3, test7) 
-{
-  Automata A;
-  A.on();
-  A.coin(100);
-  int result = A.choice(1);
-  EXPECT_EQ(30, result);
+TEST(task3, test7) {
+    Automata a = Automata();
+    a.choice(5);
+    a.choice(0);
+    a.coin(150);
+    int result = a.choice(1);
+    EXPECT_EQ(0, result);
 }

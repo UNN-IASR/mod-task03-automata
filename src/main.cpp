f2c98096bf29505609c5ae663e2a7dac4a9cd322
Aniloren <80814419+Aniloren@users.noreply.github.com>
2021-05-07 14:51:24 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@ int main()
     Automata aut = Automata();
     int change = 0;
     aut.on();
-    aut.coin(70);
+    aut.coin(80);
     aut.choice(1);
     change = aut.cancel();
     aut.coin(100);
     aut.choice(2);
-    aut.coin(180);
-    aut.choice(2);
+    aut.coin(120);
+    aut.choice(3);
     change = aut.cancel();
     aut.off();
 }

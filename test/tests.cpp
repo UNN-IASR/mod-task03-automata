dcdb1ef97f2581697ac115e6fb051cb8c6369a00
Aniloren <80814419+Aniloren@users.noreply.github.com>
2021-05-08 18:57:25 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,57 +1,39 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
 
-TEST(task1, test1) {
-  Automata aut = Automata();
-  aut.cancel();
-  int result = aut.getState();
-  EXPECT_EQ(0,result);
+TEST(task, test1)
+{
+    Automata coffee = Automata();
+    coffee.coin(20);
+    coffee.choice(1);
+    int result = coffee.getState();
+    EXPECT_EQ(0, result);
+}
+TEST(task, test2)
+{
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.coin(20);
+    coffee.choice(1);
+    int result = coffee.getState();
+    EXPECT_EQ(1, result);
 }
 
-TEST(task1, test2) {
-  Automata aut = Automata();
-  aut.on();
-  aut.coin(0);
-  int result = aut.getState();
-  EXPECT_EQ(2,result);
+TEST(task, test3)
+{
+    Automata coffee= Automata();
+    coffee.on();
+    coffee.off();
+    int result = coffee.getState();
+    EXPECT_EQ(0, result);
 }
 
-TEST(task2, test3) {
-  Automata aut = Automata();
-  aut.on();
-  aut.cancel();
-  int result = aut.getState();
-  EXPECT_EQ(1,result);
-}
-
-TEST(task2, test4) {
-  Automata aut = Automata();
-  aut.on();
-  aut.coin(20);
-  int result = aut.choice(10);
-  EXPECT_EQ(20,result);
-}
-
-TEST(task3, test5) {
-  Automata aut = Automata();
-  aut.on();
-  aut.coin(300);
-  int result = aut.choice(2);
-  EXPECT_EQ(0,result);
-}
-
-TEST(task3, test6) {
-  Automata aut = Automata();
-  aut.on();
-  aut.coin(300);
-  aut.choice(2);
-  int result = aut.cancel();
-  EXPECT_EQ(100,result);
-}
-TEST(task3, test7) {
-  Automata aut = Automata();
-  aut.on();
-  aut.coin(20);
-  int result = aut.choice(1);
-  EXPECT_EQ(20,result);
+TEST(task, test4)
+{
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.coin(10);
+    coffee.coin(30);
+    int result = coffee.getState();
+    EXPECT_EQ(2, result);
 }
